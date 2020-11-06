4aa40a9155a8369b44c7b20a13df2f4d69885627
Jordan Scales <scalesjordan@gmail.com>
2020-11-05 20:23:49 -0500

    move transform into a module

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let fs = require("fs");
 let pegjs = require("pegjs");
 let escodegen = require("escodegen");
+let transform = require("./transform");
 
 let parser = pegjs.generate(fs.readFileSync("./basic2020.pegjs", "utf-8"));
 
@@ -13,48 +14,7 @@ let program = `
   END
 `;
 
-let transformIfs = (node) =>
-  node &&
-  node.type === "CallExpression" &&
-  node.callee.type === "Identifier" &&
-  /^print$/i.test(node.callee.name)
-    ? {
-        ...node,
-        callee: {
-          type: "MemberExpression",
-          object: {
-            type: "Identifier",
-            name: "console",
-          },
-          property: {
-            type: "Identifier",
-            name: "log",
-          },
-        },
-      }
-    : node;
-
-let transforms = [transformIfs];
-let applyTransforms = (item) => transforms.reduce((acc, f) => f(acc), item);
-
-let transform = (ast) => {
-  ast = applyTransforms(ast);
-
-  if (!ast) {
-    return ast;
-  } else if (ast.type) {
-    for (let field in ast) {
-      ast[field] = transform(ast[field]);
-    }
-    return ast;
-  } else if (Array.isArray(ast)) {
-    return ast.map((item) =>
-      transform(transforms.reduce((acc, f) => f(acc), item))
-    );
-  } else {
-    return transforms.reduce((acc, f) => f(acc), ast);
-  }
-};
-
-// console.log(JSON.stringify(parser.parse(program), null, 2));
-console.log(escodegen.generate(transform(parser.parse(program))));
+let parsed = parser.parse(program);
+let transformed = transform(parsed);
+let generated = escodegen.generate(transformed);
+console.log(generated);
---
+++ transform.js
@@ -0,0 +1,42 @@
+let transformIfs = (node) =>
+  node &&
+  node.type === "CallExpression" &&
+  node.callee.type === "Identifier" &&
+  /^print$/i.test(node.callee.name)
+    ? {
+        ...node,
+        callee: {
+          type: "MemberExpression",
+          object: {
+            type: "Identifier",
+            name: "console",
+          },
+          property: {
+            type: "Identifier",
+            name: "log",
+          },
+        },
+      }
+    : node;
+
+let transforms = [transformIfs];
+let applyTransforms = (item) => transforms.reduce((acc, f) => f(acc), item);
+
+module.exports = function transform(ast) {
+  ast = applyTransforms(ast);
+
+  if (!ast) {
+    return ast;
+  } else if (ast.type) {
+    for (let field in ast) {
+      ast[field] = transform(ast[field]);
+    }
+    return ast;
+  } else if (Array.isArray(ast)) {
+    return ast.map((item) =>
+      transform(transforms.reduce((acc, f) => f(acc), item))
+    );
+  } else {
+    return transforms.reduce((acc, f) => f(acc), ast);
+  }
+};
