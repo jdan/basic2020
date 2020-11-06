0688ab18581f075a6750441e0634a08656c2b475
Jordan Scales <scalesjordan@gmail.com>
2020-11-05 20:20:06 -0500

    transforms

--- main.js
+++ main.js
@@ -13,5 +13,48 @@ let program = `
   END
 `;
 
+let transformIfs = (node) =>
+  node &&
+  node.type === "CallExpression" &&
+  node.callee.type === "Identifier" &&
+  /^print$/i.test(node.callee.name)
+    ? {
+        ...node,
+        callee: {
+          type: "MemberExpression",
+          object: {
+            type: "Identifier",
+            name: "console",
+          },
+          property: {
+            type: "Identifier",
+            name: "log",
+          },
+        },
+      }
+    : node;
+
+let transforms = [transformIfs];
+let applyTransforms = (item) => transforms.reduce((acc, f) => f(acc), item);
+
+let transform = (ast) => {
+  ast = applyTransforms(ast);
+
+  if (!ast) {
+    return ast;
+  } else if (ast.type) {
+    for (let field in ast) {
+      ast[field] = transform(ast[field]);
+    }
+    return ast;
+  } else if (Array.isArray(ast)) {
+    return ast.map((item) =>
+      transform(transforms.reduce((acc, f) => f(acc), item))
+    );
+  } else {
+    return transforms.reduce((acc, f) => f(acc), ast);
+  }
+};
+
 // console.log(JSON.stringify(parser.parse(program), null, 2));
-console.log(escodegen.generate(parser.parse(program)));
+console.log(escodegen.generate(transform(parser.parse(program))));
