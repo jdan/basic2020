971c2f4cb8e02af0fa52c774defe2bd872cd959e
Jordan Scales <scalesjordan@gmail.com>
2020-11-08 15:49:55 -0500

    run from command line

--- lang/main.js
+++ lang/main.js
@@ -12,3 +12,25 @@ function run(code) {
 }
 
 module.exports = run;
+
+if (require.main === module) {
+  let code = "";
+  process.stdin.on("data", (line) => {
+    code += line.toString();
+  });
+  process.stdin.on("end", () => {
+    let parser = pegjs.generate(fs.readFileSync("./basic2020.pegjs", "utf-8"));
+    let parsed = parser.parse(code);
+    let transformed = transform(parsed);
+    let generated = escodegen.generate(transformed);
+
+    if (process.argv.find((i) => i === "-v")) {
+      console.log("--- BEGIN GENERATED OUTPUT ---");
+      console.log(generated);
+      console.log("---- END GENERATED OUTPUT ----\n");
+    }
+
+    // todo: don't log, require PRINT()
+    console.log(eval(generated));
+  });
+}
